6e19f52be6b6c19739538ca0fe43da8379c0a8f7
Tookai <tybo.orcel@gmail.com>
2023-10-10 23:07:41 +0200

    setup simple transition

---
+++ src/data/data.ts
@@ -0,0 +1,219 @@
+export const data = [
+	{
+		id: "0c4b7d5e-e484-418a-8ec2-838623abff5a",
+		imageUrl:
+			"https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "9173ac86-2032-40e7-8fea-604088175186",
+		imageUrl:
+			"https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "1cd4f1fd-d43c-49bd-aeda-93753e0b13be",
+		imageUrl:
+			"https://images.pexels.com/photos/1743165/pexels-photo-1743165.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "b888e705-cf2d-4de0-b974-f323606b351b",
+		imageUrl:
+			"https://images.pexels.com/photos/1450082/pexels-photo-1450082.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "a194cca0-dc54-4126-a0b9-9d47fc8eefde",
+		imageUrl:
+			"https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "8db47c4d-10d8-4ab4-838e-38ee28ae4a8f",
+		imageUrl:
+			"https://images.pexels.com/photos/1486974/pexels-photo-1486974.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "803fcb0f-527b-4d21-a5ae-2ad36e6263c0",
+		imageUrl:
+			"https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "d73e7389-dcfe-4e60-94b8-649773ef8e72",
+		imageUrl:
+			"https://images.pexels.com/photos/2437291/pexels-photo-2437291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "fb2546f6-1c14-4584-a5be-2d9f44c65691",
+		imageUrl:
+			"https://images.pexels.com/photos/2896668/pexels-photo-2896668.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "2687d3e7-8aec-422f-9234-bfe255ae130f",
+		imageUrl:
+			"https://images.pexels.com/photos/1624438/pexels-photo-1624438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "3e473d5c-6ae2-49ff-967a-02b1346ee091",
+		imageUrl:
+			"https://images.pexels.com/photos/808465/pexels-photo-808465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "ba6d8741-d919-45b4-8e52-f66408de137c",
+		imageUrl:
+			"https://images.pexels.com/photos/1670187/pexels-photo-1670187.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "a2742e78-af3b-41fa-bca4-3295431698e4",
+		imageUrl:
+			"https://images.pexels.com/photos/1586298/pexels-photo-1586298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "c06723c1-aae5-40a2-b6d1-3e0ee0b06c05",
+		imageUrl:
+			"https://images.pexels.com/photos/675764/pexels-photo-675764.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "faecdaf8-01df-440c-837b-efb64979ab70",
+		imageUrl:
+			"https://images.pexels.com/photos/730981/pexels-photo-730981.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "8c970c4c-7ab7-4dd1-b9c9-5a0ebdd9e8ca",
+		imageUrl:
+			"https://images.pexels.com/photos/1266810/pexels-photo-1266810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "cc5e8ba4-7979-4fae-9cd7-f57ef41b7895",
+		imageUrl:
+			"https://images.pexels.com/photos/2387418/pexels-photo-2387418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "2547bee7-b32c-4e68-b0da-0169dc6e8899",
+		imageUrl:
+			"https://images.pexels.com/photos/2325446/pexels-photo-2325446.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "93e9716a-c6d6-4a5e-95a2-f257d098a66f",
+		imageUrl:
+			"https://images.pexels.com/photos/2613946/pexels-photo-2613946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "579bd70c-47b2-4e76-8ac6-fda4eb013752",
+		imageUrl:
+			"https://images.pexels.com/photos/325807/pexels-photo-325807.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "07f0be6e-bfb0-42a5-b33d-16ad0201ceef",
+		imageUrl:
+			"https://images.pexels.com/photos/2832034/pexels-photo-2832034.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "26c63146-ed4f-43a2-9f32-b0888e7ccc9d",
+		imageUrl:
+			"https://images.pexels.com/photos/1785493/pexels-photo-1785493.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "26acd172-9d2c-4e98-a7ba-eaf34a71fb22",
+		imageUrl:
+			"https://images.pexels.com/photos/2365465/pexels-photo-2365465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "56b96b19-5d8f-496d-91cf-55a3a9d87cb2",
+		imageUrl:
+			"https://images.pexels.com/photos/1809644/pexels-photo-1809644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "42ea9873-8be8-4164-8ae0-789e558b7d38",
+		imageUrl:
+			"https://images.pexels.com/photos/633198/pexels-photo-633198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "ec051ca2-0f26-4e95-b35b-eb4fc06f42bb",
+		imageUrl:
+			"https://images.pexels.com/photos/789382/pexels-photo-789382.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "ca690d44-0d05-4a92-b013-9a738dadca81",
+		imageUrl:
+			"https://images.pexels.com/photos/841303/pexels-photo-841303.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "5c6c0b3a-abd8-46bd-b428-edc01f75d31c",
+		imageUrl:
+			"https://images.pexels.com/photos/849403/pexels-photo-849403.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "26669697-151a-4965-ba8c-8b4856752250",
+		imageUrl:
+			"https://images.pexels.com/photos/910309/pexels-photo-910309.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "7e6bc51a-0b98-4098-a3b9-3eda24e5263f",
+		imageUrl:
+			"https://images.pexels.com/photos/910310/pexels-photo-910310.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+	{
+		id: "4d89b9ca-0be6-4cf1-9b7b-5fbc6aaab8af",
+		imageUrl:
+			"https://images.pexels.com/photos/991422/pexels-photo-991422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+	},
+]
